4d00d9495edc1f66e1d45816f2c6615f46c5eda6
Warren <rabbitsinwarrens@gmail.com>
2023-08-18 09:30:15 +1000

    feat(constants): add paths for datasets

---
+++ constants.py
@@ -0,0 +1,6 @@
+PERSONAL_INFO = "data/personal_info.csv"
+VEHICLES = "data/vehicles.csv"
+EMPLOYMENT = "data/employment.csv"
+UPDATE_STATUS = "data/update_status.csv"
+
+FILE_NAMES = PERSONAL_INFO, VEHICLES, EMPLOYMENT, UPDATE_STATUS
